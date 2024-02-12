bff58191613e36a1022993ecd78cdb5143bda80a
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-12 20:51:53 +0000

    movies template insert gui added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,22 @@
 class MoviesController < ApplicationController
+
+  def create
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    m.save
+    # Retrieve the user's inputs from params
+    # Create a record in the movie table
+    # Populate each column with the user input
+    # Save 
+
+    # Redirect the user back to the /movies URL
+    redirect_to("/movies")
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
